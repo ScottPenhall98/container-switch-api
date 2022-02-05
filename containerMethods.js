abf19d7dc9cd45a5fb9ba84dcbcc5d1ffb81ad01
ScottPenhall98 <scottpenhall98@gmail.com>
2022-02-05 18:54:39 +1300

    have methods working as intended

--- containerMethods.js
+++ containerMethods.js
@@ -1,21 +1,85 @@
 import * as child from 'child_process';
 
-const getData = async (req, res, next) => {
-    let error = false;
-    let message = await new Promise((resolve, reject) => {
-        child.exec('ls -l aaa', (err, stdout, stderr) => {
+const status = async () => {
+    return new Promise((resolve, reject) => {
+        child.exec(`docker inspect ${process.env.CONTAINER_NAME} | grep State -A 12`, (err, stdout, stderr) => {
             if (err) {
-                error = true;
-                resolve(err)
+                resolve(JSON.parse('{"Successful": false, "Error": "Error while getting container status"}'));
+                return
             }
-            resolve(stdout)
+            let data = stdout.split(',')
+            data.pop()
+            resolve(JSON.parse(`{"Successful": true, ${data.join(',')}}`))
         })
     })
-    return res.status(error ? 404: 200).json({
-        message : message
+}
+
+const checkStatus = async (req, res, next) => {
+    await status().then(message => {
+        res.status(200).json({
+            message: message
+        })
+    });
+
+}
+
+const startContainer = async (req, res, next) => {
+    await status().then( async status => {
+        if (status.Successful) {
+            if (!status.State.Running) {
+                let message = await new Promise((resolve, reject) => {
+                    child.exec(`docker start ${process.env.CONTAINER_NAME}`, (err, stdout, stderr) => {
+                        if (err) {
+                            resolve(err)
+                        }
+                        resolve('Container started')
+                    })
+                })
+
+                return res.status(200).json({
+                    message: message
+                })
+            }
+            return res.status(204).json({
+                message: 'Container is already running'
+            })
+        }
+        return res.status(204).json({
+            message: 'Cannot retrieve container status'
+        })
+    })
+}
+
+
+const stopContainer = async (req, res, next) => {
+    await status().then( async status => {
+        if (status.Successful) {
+            if (status.State.Running) {
+                let message = await new Promise((resolve, reject) => {
+                    child.exec(`docker stop ${process.env.CONTAINER_NAME}`, (err, stdout, stderr) => {
+                        if (err) {
+                            resolve(err)
+                        }
+                        resolve('Container stopped')
+                    })
+                })
+
+                return res.status(200).json({
+                    message: message
+                })
+            }
+            return res.status(204).json({
+                message: 'Container has already stopped'
+            })
+        }
+        return res.status(204).json({
+            message: 'Cannot retrieve container status'
+        })
     })
 }
 
 export default {
-    getData
+    checkStatus,
+    startContainer,
+    stopContainer
 };
--- methods.js
+++ methods.js
@@ -3,6 +3,8 @@ import containerMethods from "./containerMethods.js";
 
 const router = express.Router()
 
-router.get('/data', containerMethods.getData);
+router.get('/status', containerMethods.checkStatus);
+router.get('/start', containerMethods.startContainer);
+router.get('/stop', containerMethods.stopContainer);
 
 export default router;
